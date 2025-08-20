b01abe0a11ed0da7528ac1d5053b07631e652ba7
Eric Sears <ericsears13@gmail.com>
2025-08-20 16:25:58 -0700

    added Eric's name

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// Author Your Name
+// Author : Eric Sears
 // Description: Hello World in C++
 //============================================================
 
@@ -8,34 +8,14 @@ int hello1Function(string name1);
 int hello2Function(string name2);
 int main()
 {
-    string name1="your name1";
+    string name1 = "Eric Sears";
     hello1Function(name1);
-    string name2="your name2";
-    hello2Function(name2);
-    return 0;
-}
 
-/*
-* hello1Function.cpp
-*
-*/
-#include <iostream>
-using namespace std;
-int hello1Function(string name1)
-{
-cout << "hello World " << name1 << endl;
-return 0;
-}
-Lab 2
-hello1Function
-/*
-* hello2Function.cpp
-*
-*/
-#include <iostream>
-using namespace std;
-int hello2Function(string name2)
-{
-cout << "hello World " << name2 << endl;
-return 0;
+    string name2 = "your name2";
+    hello2Function(name2);
+
+    string name3 = "your name3";
+    hello2Function(name3);
+
+    return 0;
 }
