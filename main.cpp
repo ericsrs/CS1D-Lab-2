4843ff831b50eb8bf4554d54542412024b286a01
Eric Sears <ericsears13@gmail.com>
2025-08-20 16:29:10 -0700

    added files for hello functions

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int hello1Function(string name1);
 int hello2Function(string name2);
 int main()
 {
-    string name1="Sear Sears";
+    string name1="Eric Sears";
     hello1Function(name1);
     string name2="Kevin Azarbal";
     hello2Function(name2);
@@ -19,17 +19,7 @@ int main()
     return 0;
 }
 
-/*
-* hello1Function.cpp
-*
-*/
-#include <iostream>
-using namespace std;
-int hello1Function(string name1)
-{
-cout << "hello World " << name1 << endl;
-return 0;
-}
+
 // Lab 2
 // hello1Function
 /*
@@ -40,6 +30,6 @@ return 0;
 using namespace std;
 int hello2Function(string name2)
 {
-cout << "hello World " << name2 << endl;
-return 0;
+    cout << "hello World " << name2 << endl;
+    return 0;
 }
